e6f42d987ba104ea2c61ec09f2ba8501926c7904
506-30 <506-30@506-30-PC>
2017-08-21 11:32:29 +0900

    2번째 수정

--- 170821/src/Main02.java
+++ 170821/src/Main02.java
@@ -1,10 +1,9 @@
 
-public class Main {
+public class Main02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("1번째 커밋");
-		System.out.println("코드 수정");
+
 	}
 
 }
